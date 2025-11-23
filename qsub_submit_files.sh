6b4aaa041f57dc0cbe6c0ba9ec99b7ab3b6279da
Wanzhe <103027185+WanzheUTokyo@users.noreply.github.com>
2025-11-23 13:09:44 +0900

    Delete qsub_submit_files.sh

--- qsub_submit_files.sh
+++
@@ -1,6 +0,0 @@
-#$ -S /bin/bash
-qsub -l a100=1,s_vmem=200G \
-     -N All_target_48h_seed2025 \
-     -e /home/wanzhe/OmniTFT/qsub_output/test \
-     -o /home/wanzhe/OmniTFT/qsub_output/test \
-     qsub_run_script.sh
