567ae286ddb2fe579199cb8ef39a74b8c0bd4b04
Wanzhe <103027185+WanzheUTokyo@users.noreply.github.com>
2025-11-23 13:25:29 +0900

    Add files via upload

---
+++ qsub_run_script.sh
@@ -0,0 +1,16 @@
+#$ -S /bin/bash
+
+# 加载 Conda 配置
+# source /home/wanzhe/anaconda3/etc/profile.d/conda.sh
+
+# 激活指定的 Conda 环境
+# conda activate tfticu
+source /home/wanzhe/anaconda3/bin/activate tf213
+# 运行 Python 脚本
+python /home/wanzhe/OmniTFT/train_pepline.py . yes
+echo "Script run ended"
+# 退出 Conda 环境
+conda deactivate
+
+echo "Script executed successfully"
+
