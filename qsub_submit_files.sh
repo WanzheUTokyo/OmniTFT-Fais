eb51cf6fe016cba44040c0a8deeef8f01627d3a4
Wanzhe <103027185+WanzheUTokyo@users.noreply.github.com>
2025-11-23 13:24:08 +0900

    Add files via upload

---
+++ qsub_submit_files.sh
@@ -0,0 +1,6 @@
+#$ -S /bin/bash
+qsub -l h100=1,s_vmem=200G \
+     -N All_target_48h_seed2025 \
+     -e /home/wanzhe/OmniTFT/qsub_output/test \
+     -o /home/wanzhe/OmniTFT/qsub_output/test \
+     qsub_run_script.sh
